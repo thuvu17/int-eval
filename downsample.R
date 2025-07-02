219e5cb6d7b92a5b01031b04676ebcfc04c80be8
Thu Vu <tnv2002@nyu.edu>
2025-07-02 17:44:30 -0400

    create balanced two batch pbmc

--- downsample.R
+++ downsample.R
@@ -6,3 +6,19 @@ shared_celltypes <- intersect(valid_3p, valid_5p)
 
 pbmc_3p <- subset(pbmc_3p, subset = celltype %in% shared_celltypes)
 pbmc_5p <- subset(pbmc_5p, subset = celltype %in% shared_celltypes)
+
+# Then, randomly select only 200 cells in each cell type
+downsample <- function(seurat_obj, group_var = "celltype", target_n = 200) {
+  celltypes <- table(seurat_obj[[group_var]][, 1])
+  valid_types <- names(celltypes[celltypes >= target_n])
+  
+  selected_cells <- unlist(lapply(valid_types, function(ct) {
+    cells <- rownames(seurat_obj@meta.data[seurat_obj@meta.data[[group_var]] == ct, ])
+    sample(cells, target_n)
+  }))
+  
+  subset(seurat_obj, cells = selected_cells)
+}
+
+pbmc_3p.balanced <- downsample(pbmc_3p)
+pbmc_5p_balanced <- downsample(pbmc_5p)
